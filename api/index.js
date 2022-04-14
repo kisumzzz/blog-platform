618962048b7a4d0784ebf4d8724d69ed5fd7af62
Anqi Zuo <azuo@ace-atlanta.com>
2022-04-13 23:23:53 -0400

    upload file api

--- api/index.js
+++ api/index.js
@@ -4,16 +4,31 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const { env } = require("process");
-const authRoute = require("./routes/auth")
-const userRoute = require("./routes/users")
-const postRoute = require("./routes/posts")
-const cateRoute = require("./routes/categories")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const cateRoute = require("./routes/categories");
+const multer = require("multer");
 
 
-dotenv.config();
+dotenv.config(); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(console.log('Connect to MangoDB'));
+
+// middleware of files upload api
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null,"images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+const upload = multer({ storage:storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded successfully.");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
